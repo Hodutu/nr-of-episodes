555fab17eeca1d9641886657d5e365e1dbb084df
Michał Budzyński <michal@virtualdesign.pl>
2014-10-13 01:01:00 +0100

    :poodle: if no episodes left, return error callback

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 'use strict';
 
 var wi = require('wiki-infobox');
-var assert = require('assert');
 
+var reqTry = 0;
 var noe = function(title, cb) {
-  cb(0);
-  wi();
+  reqTry++;
+  wi(title, 'en', function(err, infobox) {
+    if (!infobox && title.indexOf('series') === -1) {
+      cb(new Error('No Episodes found :('));
+      return;
+    }
+    cb(err, infobox.num_episodes);
+  });
 };
 
-noe('Modern Family', function(episodes) {
-  assert(episodes, 126);
+var shows = [
+  {
+    title: 'Modern Family',
+    episodes: 126
+  },
+  {
+    title: 'Dollhouse',
+    episodes: 26
+  }
+];
+
+var nr = 1;
+
+noe(shows[nr].title, function(err, episodes) {
+  if (err){
+    console.log(err);
+    return;
+  }
+
+  console.log(episodes);
 });
 
 module.exports = noe;
