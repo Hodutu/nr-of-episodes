1744348f2bfd52e738dfacdf1ea211d736149083
Michał Budzyński <michal@virtualdesign.pl>
2014-10-13 01:01:00 +0100

    :water_buffalo: move test to index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = function(title, cb) {
+var wi = require('wiki-infobox');
+var assert = require('assert');
+
+var noe = function(title, cb) {
   cb(0);
+  wi();
 };
+
+noe('Modern Family', function(episodes) {
+  assert(episodes, 126);
+});
+
+module.exports = noe;
