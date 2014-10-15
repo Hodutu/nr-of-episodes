815b5041ac8cc4ad94b3bfa0a32c330892ce6d40
Michał Budzyński <michal@virtualdesign.pl>
2014-10-15 01:01:00 +0100

    :frowning: tests added

--- tests/nr-of-episodes-tests.js
+++ tests/nr-of-episodes-tests.js
@@ -1,7 +1,12 @@
 'use strict';
 
-var assert = require('assert');
-//var noe = require('../');
+//var assert = require('assert');
+var noe = require('../');
 
-// Search by title
-assert(true);
+var list = require('../../tv-series/data/data.js');
+
+list.forEach(function(show){
+  noe(show.title, function(e, d){
+    console.log(show.title + ' episodes: ' + d);
+  });
+});
