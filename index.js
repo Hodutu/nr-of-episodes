c381f4e90acef345641beade916c45a4d3cae56c
Michał Budzyński <michal@virtualdesign.pl>
2014-10-15 01:01:00 +0100

    :church: simple object sypport, not only arrays

--- index.js
+++ index.js
@@ -15,27 +15,32 @@ var noe = function(title, cb) {
       return;
     }
 
-    console.log(infobox.num_episodes);
+    console.log(title);
+    var episodes = infobox.num_episodes;
 
-    var episodes = infobox.num_episodes.filter(function(element) {
-      var parsed = parseInt(element.value, 10);
-      return element.type === 'text' &&
-        typeof parsed === 'number' &&
-        !Number.isNaN(parsed);
-    });
+    if (Array.isArray(infobox.num_episodes)) {
+      episodes = infobox.num_episodes.filter(function(element) {
+        var parsed = parseInt(element.value, 10);
 
-    cb(err, parseInt(episodes[0].value, 10));
+        return element.type === 'text' &&
+          typeof parsed === 'number' &&
+          !Number.isNaN(parsed);
+      }).pop();
+    };
+
+    episodes = episodes && episodes.value ? parseInt(episodes.value, 10) : 0;
+    cb(err, episodes);
   });
 };
 
 
-noe('MacGyver', function(err, episodes) {
-  if (err){
-    console.log(err);
-    return;
-  }
-
-  console.log(episodes);
-});
+// noe('MacGyver', function(err, episodes) {
+//   if (err){
+//     console.log(err);
+//     return;
+//   }
+//
+//   console.log(episodes);
+// });
 
 module.exports = noe;
